d7b0c9a3c4d30beb571be9e9709f5d95896e441c
SpeedyLom <speedylomuk@gmail.com>
2024-03-02 13:26:54 +0000

    Update paths to work with public directory set to root

--- public/index.php
+++ public/index.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark" />
-    <link rel="stylesheet" href="css/pico.min.css">
+    <link rel="stylesheet" href="/css/pico.min.css">
     <title>Hello world!</title>
-    <link rel="icon" type="image/x-icon" href="images/favicon.png">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
 </head>
 <body>
 <main class="container">
     <h1 id="hello">Hello world!</h1>
 </main>
-<script type="application/javascript" src="js/index.js"></script>
+<script type="application/javascript" src="/js/index.js"></script>
 </body>
 </html>
